976692908d19d61c9e9730822c6ddc425bee7476
Mourtada <mourtadam21@gmail.com>
2024-06-11 17:00:12 +0200

    ajouter les routes pour bieres et stock

--- database/migrations/2024_06_10_134303_create_stocks_table.php
+++ database/migrations/2024_06_10_134303_create_stocks_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('stock', function (Blueprint $table) {
+        Schema::create('stocks', function (Blueprint $table) {
             $table->id();
             $table->foreignId('biere_id')
                 ->constrained()
